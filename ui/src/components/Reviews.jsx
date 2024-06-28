3f4539f52303e3b3bb58f236fb25bc9cb42d678a
A. Samuel <aaron.psamuel@gmail.com>
2024-06-28 14:52:02 -0400

    chore: consistently wrap Outlet displays in Stacks

--- ui/src/components/Reviews.jsx
+++ ui/src/components/Reviews.jsx
@@ -1,18 +1,33 @@
+import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 export default function Reviews() {
   return (
-    <Box>
-      <Typography
-        variant='h6'
-        component='div'
-        sx={{
-          fontFamily: 'Roboto',
-          fontSize: '2rem',
-          fontWeight: 700,
-          letterSpacing: '0.3em'
-        }}> Truck Reviews. Coming Soon!  </Typography>
-    </Box>
+    <Stack
+      sx={{
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignContent: 'center',
+        borderRadius: (theme) => theme.shape.borderRadius,
+        border: (theme) =>
+          `1px solid ${theme.palette.primary.contrastText}`,
+        // height: '100%',
+      }}
+    >
+      <Box>
+        <Typography
+          variant='h6'
+          component='div'
+          sx={{
+            fontFamily: 'Roboto',
+            fontSize: '2rem',
+            fontWeight: 700,
+            letterSpacing: '0.3em'
+          }}> Truck Reviews. Coming Soon!  </Typography>
+      </Box>
+    </Stack>
+
   );
 }
