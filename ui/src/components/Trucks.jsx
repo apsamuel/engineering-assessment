fb1753e4974c9c6ba07a429d5cda645e471466d1
A. Samuel <aaron.psamuel@gmail.com>
2024-06-28 14:52:20 -0400

    chore: cleanup

--- ui/src/components/Trucks.jsx
+++ ui/src/components/Trucks.jsx
@@ -2,32 +2,33 @@
 import React from 'react';
 import 'leaflet/dist/leaflet.css';
 // import SanFrancLogo from '../assets/sanfran.svg';
+import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 // eslint-disable-next-line no-unused-vars
-import L from "leaflet";
+import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import { useOutletContext } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-
 CustomMarker.propTypes = {
   position: PropTypes.arrayOf(PropTypes.number).isRequired,
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-// function CustomMarker({ position, children }: CustomMarkerProps)
 function CustomMarker({ position, children }) {
-  const map = useMap()
-  console.log('map', map)
+  // eslint-disable-next-line no-unused-vars
+  const map = useMap();
   return (
     <div>
       <Marker
         // icon={icon}
         position={position}
-      >{children}</Marker>
+      >
+        {children}
+      </Marker>
     </div>
-  )
+  );
 }
 export default function Trucks() {
   const {
@@ -57,45 +58,37 @@ export default function Trucks() {
           position={[truck.latitude, truck.longitude]}
         >
           <Popup>
-            <Typography
-              variant={'h6'}
-            >
-              {truck.applicant}
-            </Typography>
-            <Typography
-              variant={'subtitle1'}
-            >
-              {truck.address}
-            </Typography>
+            <Typography variant={'h6'}>{truck.applicant}</Typography>
+            <Typography variant={'subtitle1'}>{truck.address}</Typography>
           </Popup>
         </CustomMarker>
-      )
-    })
-  }
+      );
+    });
+  };
   return (
-
-    <Box
-      sx={{
-        border: (theme) => `1px solid ${theme.palette.primary.contrastText}`,
-        width: '100%',
-        height: '100%',
-      }}
-    >
-      <MapContainer
-        style={{
-          width: '50vw',
-          height: '100%',
+    <Stack>
+      <Box
+        sx={{
+          border: (theme) => `1px solid ${theme.palette.primary.contrastText}`,
+          width: '100%',
+          height: '100%'
         }}
-        center={[37.777399395507075, -122.41982705224395]}
-        zoom={10}
       >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
+        <MapContainer
+          style={{
+            width: '50vw',
+            height: '100%'
+          }}
+          center={[37.777399395507075, -122.41982705224395]}
+          zoom={10}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
           {createCoordinateMarkers(filterTrucks)}
-      </MapContainer>
-
-    </Box>
+        </MapContainer>
+      </Box>
+    </Stack>
   );
 }
