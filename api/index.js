3eb299e6fa8de28517cf2ae5f581f7cb9c980ab9
A. Samuel <aaron.psamuel@gmail.com>
2024-06-28 14:50:14 -0400

    chore: modern http servers log client data, so should we

--- api/index.js
+++ api/index.js
@@ -9,6 +9,13 @@ const port = process.env.API_PORT || 8080;
 const apiLogger = (req, res, next) => {
   console.log(`api-${req.method.toLowerCase()}`, JSON.stringify({
     timestamp: new Date().toISOString(),
+    client: {
+      ip: req.ip,
+      host: req.headers.host,
+      origin: req.headers.origin,
+      accept: req.headers.accept,
+      agent: req.headers['user-agent'],
+    },
     method: req.method,
     url: req.url,
     query: req.query || 'no query',
@@ -24,19 +31,20 @@ app.get('/', (req, res) => {
 
 app.get('/api', (req, res) => {
   res.json({
-    message: 'Welcome to the API'
+    message: 'Welcome to the API',
+    usage: {
+      '/api': 'this message',
+      '/api/trucks': 'all trucks',
+      '/api/trucks/categories': 'all truck categories',
+      '/api/trucks/vendors': 'all truck vendors',
+      '/api/trucks/vendors/:vendor': 'all trucks for a vendor',
+      '/api/trucks/vendors/:vendor/categories': 'all categories for a vendor',
+    }
   })
 })
 
 app.get('/api/trucks', async (req, res) => {
   apiLogger(req, res);
-  // console.log(`api-${req.method.toLowerCase()}`, JSON.stringify({
-  //   timestamp: new Date().toISOString(),
-  //   url: req.url,
-  //   query: req.query || 'no query',
-  //   params: req.params || 'no params',
-  //   body: req.body,
-  // }, null, 2))
   const query = req.query;
   let { format } = query;
   if (!format) format = 'csv'
@@ -48,14 +56,7 @@ app.get('/api/trucks', async (req, res) => {
   res.json(data)
 })
 
-// app.get('/api/trucks/vendors', async (req, res) => {
-//   apiLogger(req, res);
-//   const compiled = await compileData();
-//   const headers = compiled[0];
-//   const data = compiled.slice(1, )
-//   const vendors = Array.from(new Set(data.map((truck) => truck.applicant)));
-//   res.json(vendors)
-// })
+
 
 app.get('/api/trucks/categories', async (req, res) => {
   apiLogger(req, res);
@@ -67,20 +68,24 @@ app.get('/api/trucks/categories', async (req, res) => {
     .filter(Boolean)
     .join(';')
     .split(new RegExp('[;:.]', 'g'))
-    .map((item) => item.trim().toLowerCase().replace( /[/]/, ' & '  ))
-    // .map((item) => {
-    //   return item === 'soft serve ice cream & frozen virgin daiquiris' ? [
-    //     'soft serve ice cream',
-    //     'frozen virgin daiquiris'
-    //   ] : item
-    // })
-    .map((item) => item.replace('all types of food except for bbq on site per fire safety', 'general'))
-    .map((item) => item.replace('asian fusion - japanese sandwiches/sliders/misubi', 'asian fusion'))
-    .map((item) => item.replace('daily rotating menus consisting of various local & organic vegetable', 'local organics'))
-    .map((item) => item.replace('pre-packaged swiches', 'packaged sandwiches'))
-    // .map((item) => item.replace(/(and)/, ''))
     .map((item) => item.trim().toLowerCase())
-    // .map((item) => item.replace('daily rotating menus consisting of various local & organic vegetables', 'local organics')
+    // General Market replacements
+    .map((item) => item.replace(
+      new RegExp(`(${[
+        'all types of food except for bbq on site per fire safety',
+        'various menu items & drinks',
+        'multiple food trucks & food types'
+      ].join('|')})`, 'g'), 'General Market'
+    ))
+    // Asian Fusion replacements
+    .map((item) => item.replace(
+      'asian fusion - japanese sandwiches/sliders/misubi', 'Asian Fusion'
+    ))
+    // Locally Sourced Organics
+    .map((item) => item.replace('daily rotating menus consisting of various local & organic vegetable', 'Local Organic'))
+    // Sandwiches
+    .map((item) => item.replace('pre-packaged swiches', 'Sandwiches'))
+    // .map((item) => item.trim().toLowerCase())
     .filter(Boolean);
   res.json(Array.from(new Set(categories)))
 })
@@ -116,9 +121,15 @@ app.get('/api/trucks/vendors/:vendor/categories', async (req, res) => {
     .filter(Boolean)
     .join(';')
     .split(new RegExp('[;:.]', 'g'))
-    .map((item) => item.trim().toLowerCase())
+    .map((item) => item.trim())
+    .map((item) => item.toLowerCase())
+    // general character replacements
+    .map((item) => item.trim().toLowerCase().replace( /[/]/, ' & '  ))
     // specific replacements
-    .map((item) => item.replace('asian fusion - japanese sandwiches/sliders/misubi', 'asian fusion'))
+    .map((item) => item.replace('all types of food except for bbq on site per fire safety', 'General Market'))
+    .map((item) => item.replace('asian fusion - japanese sandwiches/sliders/misubi', 'Asian Fusion'))
+    .map((item) => item.replace('daily rotating menus consisting of various local & organic vegetable', 'Local Organic'))
+    .map((item) => item.replace('pre-packaged swiches', 'Packaged Sandwiches'))
     .filter(Boolean);
   return res.json(Array.from(new Set(categories)))
 })
