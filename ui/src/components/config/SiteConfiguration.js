4b62abf3837ace300e0318f38e2888d3fc38f381
A. Samuel <aaron.psamuel@gmail.com>
2024-06-30 15:04:38 -0400

    chore: centralize site configuration

--- ui/src/components/config/SiteConfiguration.js
+++ ui/src/components/config/SiteConfiguration.js
@@ -1,18 +1,41 @@
+import React from 'react'
+import Visualize from '../Visualize.jsx'
+import Trucks from '../Trucks.jsx'
+import Reviews from '../Reviews.jsx'
+import About from '../About.jsx'
+
 const navigationLinks = [
   {
+    path: 'trucks',
     name: 'Trucks',
-    to: '/trucks',
-    position: 1
+    to: 'trucks',
+    element: React.createElement(Trucks),
+    position: 1,
+    children: []
   },
   {
+    path: 'viz',
     name: 'Visualize',
-    to: '/viz',
-    position: 2
+    to: 'viz',
+    element: React.createElement(Visualize),
+    position: 2,
+    children: []
   },
   {
+    path: 'reviews',
     name: 'Reviews',
-    to: '/reviews',
-    position: 3
+    to: 'reviews',
+    element: React.createElement(Reviews),
+    position: 3,
+    children: []
+  },
+  {
+    path: 'about',
+    name: 'About',
+    to: 'about',
+    element: React.createElement(About),
+    position: 4,
+    children: []
   }
 ];
 
