222f2d2913216c808bf533b9d4241cf44d9b5903
A. Samuel <aaron.psamuel@gmail.com>
2024-07-02 15:26:15 -0400

    chore: updates siteConfiguration

--- ui/src/components/config/SiteConfiguration.js
+++ ui/src/components/config/SiteConfiguration.js
@@ -1,39 +1,51 @@
 import React from 'react'
 import Visualize from '../Visualize.jsx'
-import Trucks from '../Trucks.jsx'
+import Map from '../Map.jsx'
 import Reviews from '../Reviews.jsx'
 import About from '../About.jsx'
+import MapTwoToneIcon from '@mui/icons-material/MapTwoTone';
+import AnalyticsTwoToneIcon from '@mui/icons-material/AnalyticsTwoTone';
+import GradingTwoToneIcon from '@mui/icons-material/GradingTwoTone';
+import InfoTwoToneIcon from '@mui/icons-material/InfoTwoTone';
 
 const navigationLinks = [
   {
-    path: 'trucks',
-    name: 'Trucks',
-    to: 'trucks',
-    element: React.createElement(Trucks),
+    path: 'map',
+    name: 'Map',
+    description: 'Locate Trucks',
+    to: 'map',
+    element: React.createElement(Map),
+    icon: React.createElement(MapTwoToneIcon),
     position: 1,
     children: []
   },
   {
-    path: 'viz',
-    name: 'Visualize',
-    to: 'viz',
+    path: 'data',
+    name: 'Data',
+    description: 'Data Insights',
+    to: 'data',
     element: React.createElement(Visualize),
+    icon: React.createElement(AnalyticsTwoToneIcon),
     position: 2,
     children: []
   },
   {
     path: 'reviews',
     name: 'Reviews',
+    description: 'Food Reviews',
     to: 'reviews',
     element: React.createElement(Reviews),
+    icon: React.createElement(GradingTwoToneIcon),
     position: 3,
     children: []
   },
   {
     path: 'about',
     name: 'About',
+    description: 'About Hungrèe',
     to: 'about',
     element: React.createElement(About),
+    icon: React.createElement(InfoTwoToneIcon),
     position: 4,
     children: []
   }
